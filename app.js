07383f591b1ac368d9d3a8b7c0aefa532041f6a9
Tzippy <tzippy876@gmail.com>
2025-03-20 22:00:23 +0200

    changing the function

--- app.js
+++ app.js
@@ -11,23 +11,28 @@ app.get('', (req, res) => {
     res.send('👍👍👍👍👍')
 })
 
-app.get('/Services', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.render.com/v1/Services', {
-            headers: {
-                'Authorization': `Bearer ${process.env.API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error retrieving services');
-    }
-});
+// app.get('/Services', async (req, res) => {
+//     try {
+//         const response = await axios.get('https://api.render.com/v1/Services', {
+//             headers: {
+//                 'Authorization': `Bearer ${process.env.API_KEY}`,
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error retrieving services');
+//     }
+// });
+
+app.get('/Services', (req, res)=>{
+    renderApi.auth(process.env.API_KEY);
+  renderApi.listServices({ includePreviews: 'true', limit: '20' })
+    .then( data  => {return res.status(200).send({data: data})})
+    .catch(err => console.error(err));
+})
 
 app.listen(port, () => {
     console.log(`my application is listening on http://localhost:${port}`);
 })
-
-////////////////////////
